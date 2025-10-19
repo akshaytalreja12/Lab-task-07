9c1188eaafc5f39e2b75ec9dd3aca8580c57ed63
Akshay Talreja <akshaytalreja512@gmail.com>
2025-10-20 00:17:01 +0500

    Update Qno1.c

--- Qno1.c
+++ Qno1.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
+#include <Stdio.h>
 int main(){
-	int a ,b,c=1;
-	printf("Enter the value : ");
-	scanf("%d",&a);
-	for(b=1;b<=a;b++){
-		c=c*b;
+	int roll[11]={1,2,3,3,4,4,5,5,6,7,7};
+	int roll2[11];
+	int i,j,k;
+	for(i=0;i<11;i++){
+		roll2[i]=roll[i];
+		if(roll2[i]==roll[i+1]){
+			continue;
+		}
+		else printf("%d,",roll[i]);
 	}
-	printf(" the factorial is %d",c);
+	return 0;
 }
